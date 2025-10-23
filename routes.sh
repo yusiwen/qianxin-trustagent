9867cf4049ef58d515c58b89c3ab11de06a31d57
Siwen Yu <yusiwen@gmail.com>
2025-10-23 11:44:42 +0800

    fix bypass routes priorities

--- routes.sh
+++ routes.sh
@@ -4,9 +4,11 @@ DEFAULT_GW=$(route -n | grep 'UG[ \t]' | awk '{print $2}')
 if [ -n "$DEFAULT_GW" ]; then
   if [ -n "$BYPASS_ROUTES" ]; then
     echo "Bypassing routes: $BYPASS_ROUTES ..."
+    echo '100 bypass' >> /etc/iproute2/rt_tables
+    ip rule add from all lookup bypass priority 100
     IFS=',' read -r -a routes <<< "$BYPASS_ROUTES"
     for r in "${routes[@]}"; do
-      ip route add "$r" via "$DEFAULT_GW" dev eth0 metric 50
+      ip route add "$r" via "$DEFAULT_GW" dev eth0 table bypass
     done
   fi
 fi
