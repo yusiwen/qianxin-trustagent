9ec7e79413096e361c88d1078157af45cb7ac8ec
Siwen Yu <yusiwen@gmail.com>
2026-01-19 15:31:50 +0800

    chore: refactor service management and logging for vnic udev integration
    - Remove systemd device binding and installation directives from both vnic service files
    - Add RemainAfterExit=no to service files to prevent lingering after execution
    - Change udev rules from using SYSTEMD_WANTS to directly starting services via systemctl
    - Replace logging to a file with using the system logger in both add and remove scripts
    
    Signed-off-by: Siwen Yu <yusiwen@gmail.com>

--- udev/udev-vnic-add.sh
+++ udev/udev-vnic-add.sh
@@ -2,4 +2,4 @@
 
 iptables -t nat -A POSTROUTING -o "$1" -j MASQUERADE
 iptables -t mangle -A FORWARD -o "$1" -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss 1360
-echo "iptables rules added for $1" >> /var/log/udev-vnic.log
+/usr/bin/logger "iptables rules added for $1"
--- udev/udev-vnic-remove.sh
+++ udev/udev-vnic-remove.sh
@@ -2,4 +2,4 @@
 
 iptables -t nat -D POSTROUTING -o "$1" -j MASQUERADE
 iptables -t mangle -D FORWARD -o "$1" -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss 1360
-echo "iptables rules deleted for $1" >> /var/log/udev-vnic.log
+/usr/bin/logger "iptables rules deleted for $1"
